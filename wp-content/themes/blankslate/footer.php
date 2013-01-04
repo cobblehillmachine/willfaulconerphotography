a5ffb42422cdd06da56358d8e76adb1ed43ce4f0
Justin Holt <justinphoto@gmail.com>
2013-01-03 19:18:59 -0500

    adding analytics

--- wp-content/themes/blankslate/footer.php
+++ wp-content/themes/blankslate/footer.php
@@ -16,6 +16,17 @@
 			</ul>
 			<a href="http://www.cobblehilldigital.com" title="cobble hill digital" class="cb" target="_blank">site by cobble hill</a>
 		</footer>
+		<script type="text/javascript">
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-37354511-1']);
+          _gaq.push(['_trackPageview']);
+
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+        </script>
 		<?php wp_footer(); ?>
 	</body>
 </html>
